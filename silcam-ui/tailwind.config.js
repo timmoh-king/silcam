4f446b000a8e9bad5c45f12f6c477e84f07ae428
timmoh-king <timothykariuki43@gmail.com>
2022-08-03 12:02:09 +0300

    Tailwind: added silcamwhite color

--- silcam-ui/tailwind.config.js
+++ silcam-ui/tailwind.config.js
@@ -2,13 +2,14 @@
  * @Author: Your name
  * @Date:   2022-08-03 10:31:05
  * @Last Modified by:   Your name
- * @Last Modified time: 2022-08-03 11:45:36
+ * @Last Modified time: 2022-08-03 12:00:46
  */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     colors:{
+      silcamwhite: '#FFFFFF',
       silcamblue: '#OD99FF',
       silcamyellow: '#F5AE47',
       silcamgreen: '#009689',
