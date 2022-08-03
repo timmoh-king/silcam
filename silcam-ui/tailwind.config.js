83b51c77ceddca5db44ec8aaab7410dfeef34ada
timmoh-king <timothykariuki43@gmail.com>
2022-08-03 11:48:28 +0300

    Tailwinds config: add theme and colors

--- silcam-ui/tailwind.config.js
+++ silcam-ui/tailwind.config.js
@@ -2,12 +2,24 @@
  * @Author: Your name
  * @Date:   2022-08-03 10:31:05
  * @Last Modified by:   Your name
- * @Last Modified time: 2022-08-03 10:57:02
+ * @Last Modified time: 2022-08-03 11:45:36
  */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
+    colors:{
+      silcamblue: '#OD99FF',
+      silcamyellow: '#F5AE47',
+      silcamgreen: '#009689',
+      silcamred: '#A4344B',
+      silacamdark: '#000000',
+      silcamdarkfade: '#373737',
+      silcamheadings: '#0079AD',
+      silcamtitle: '#009DDC',
+      silcambackground: '#EFFFFE',
+      silcammodule: '#F8F8F8'
+    },
     extend: {},
   },
   plugins: [],
